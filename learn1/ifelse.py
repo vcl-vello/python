629c4cbdd94bbef5e3d0c4a4c08e9497f93dac0e
vello <vello.allvo@gmail.com>
2019-02-20 13:07:30 +0700

    Fist push

---
+++ learn1/ifelse.py
@@ -0,0 +1,8 @@
+nilai = 80
+
+if nilai > 80:
+    print("Nilai A")
+elif nilai > 60:
+    print("Nilai B")
+else:
+    print("Nilai C")
---
+++ learn1/test.py
@@ -0,0 +1,8 @@
+print("hallo dunia")
+
+
+a = 2
+b = 5
+c = a + b
+
+print(c)
