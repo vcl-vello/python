1ab3e8c33f95f8ccd730ec706956662c73a5e78a
vcl-vello <vello.allvo@gmail.com>
2020-10-05 14:24:46 +0700

    tipe data

---
+++ learn2/TipeData.py
@@ -0,0 +1,20 @@
+# Tipe data integer
+data_int = 4
+print(data_int)
+print("tipe data : ", type(data_int))
+
+# Tipe data float
+data_float = 4.5
+print(data_float)
+print("tipe data : ", type(data_float))
+
+# Tipe data string
+data_string = "vello"
+print(data_string)
+print("tipe data : ", type(data_string))
+
+# Tipe data dari bahasa C
+from ctypes import c_double
+data_c_double= c_double(5.6)
+print(data_c_double)
+print("tipe data : ", type(data_c_double))
