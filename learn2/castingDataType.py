b6a1fe0b0f2c0b0720057a4c55b2eeae06eafd2c
vcl-vello <vello.allvo@gmail.com>
2020-10-05 15:07:30 +0700

    Casting Data

---
+++ learn2/castingDataType.py
@@ -0,0 +1,10 @@
+data_int = 9
+print("tipe data : ",type(data_int))
+
+data_float = float(data_int)
+data_string = str(data_int)
+data_bool = bool(data_int)
+
+print("Nilai float = ",data_float," - tipe data = ",type(data_float))
+print("Nilai string = ",data_string," - tipe data = ",type(data_string))
+print("Nilai bool = ",data_bool," - tipe data = ",type(data_bool))
